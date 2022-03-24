5be88f96c9d890ea595109afcd87ee2c2b57e483
wesolpiotr <100830679+wesolpiotr@users.noreply.github.com>
2022-03-24 07:26:36 +0000

    nieskończone wywoływanie, sprawdzanie czy liczba dodatnia

--- .ccls-cache/@home@runner@zadanie2-5/main.c
+++ .ccls-cache/@home@runner@zadanie2-5/main.c
@@ -3,14 +3,27 @@
 int liczba;
 int silnia = 1;
 
-int main(void) {
+static void silniaLiczby() {
   printf("Podaj liczbe: ");
   scanf("%d", &liczba);
+
+  if(liczba < 0) {
+    printf("Podaj liczbe dodatnia");
+    silniaLiczby();
+  }
 
   for(int i = liczba; i > 0; i--) {
     silnia *= i;
   }
 
   printf("Silnia wynosi: %d", silnia);
+  silnia = 1;
+  printf("\n");
+  silniaLiczby();
+}
+
+
+int main(void) {
+  silniaLiczby();
   return 0;
 }
